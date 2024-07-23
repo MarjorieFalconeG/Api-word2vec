10f9ec477775b9155915233d75fa5e2b1cd20985
MarjorieFalcone98 <86175382+MarjorieFalconeG@users.noreply.github.com>
2024-07-22 22:42:44 -0500

    integracion del corpus entrenado

--- REST_API/api.py
+++ REST_API/api.py
@@ -1,21 +1,23 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, jsonify, request
-from gensim.models import KeyedVectors
+from gensim.models import KeyedVectors , Word2Vec
 import os
 
-model_path = os.path.join(os.getcwd(), 'model', 'GoogleNews-vectors-negative300.bin')
-
+model_path = "model/trained_model.bin"
 app = Flask(__name__)
 
 # Cargar el modelo una vez al inicio
 model = None
 try:
-    model = KeyedVectors.load_word2vec_format(model_path, binary=True)
+    model = Word2Vec.load(model_path)
     print("Modelo cargado exitosamente")
+except FileNotFoundError:
+    print(f"Error: No se encontró el archivo {model_path}.")
 except Exception as e:
     print(f"Error al cargar el modelo: {e}")
 
 @app.route('/')
-def word2vec():
+def word2vec1():
     if model is None:
         return jsonify({"error": "El modelo no se cargó correctamente"}), 500
     
@@ -24,7 +26,7 @@ def word2vec():
         return jsonify({"error": "No se proporcionó una palabra"}), 400
 
     try:
-        similar_words = model.most_similar(positive=[word], topn=10)
+        similar_words = model.wv.most_similar(positive=[word], topn=10)
         return jsonify(similar_words)
     except KeyError:
         return jsonify({"error": f"La palabra '{word}' no se encuentra en el vocabulario del modelo"}), 400
